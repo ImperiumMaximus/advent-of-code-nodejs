d227ab8a6d72b6d2adfe18149dd368829d883279
Raffaele Fioratto <raffaele.fioratto@gmail.com>
2021-12-07 15:45:10 +0100

    day7 one sloc solutions

--- 2021/day7/challenge1.js
+++ 2021/day7/challenge1.js
@@ -1,9 +1,1 @@
-let input = require('fs').readFileSync(require('path').join(__dirname, 'input.txt')).toString().split(',').map(Number);
-let horPos;
-for (let x = Math.min(...input); x <= Math.max(...input); x++) {
-    if (input.reduce((acc, n) => (acc + ((n === x) ? 0 : ((n - x) / Math.abs(n - x)))), 0) >= 0) {
-        horPos = x;
-    }
-}
-
-console.log(input.reduce((acc, n) => acc + Math.abs(n - horPos), 0));
+console.log(Math.min(...Array.from(Array(1 + Math.max(...(input = require('fs').readFileSync(require('path').join(__dirname, 'input.txt')).toString().split(',').map(Number))) - Math.min(...input)).keys()).map(n => n + Math.min(...input)).map(pivot => input.reduce((acc, n) => acc + (n === pivot ? 0 : Math.abs(n - pivot)), 0))));
--- 2021/day7/challenge2.js
+++ 2021/day7/challenge2.js
@@ -1,18 +1,1 @@
-let input = require('fs').readFileSync(require('path').join(__dirname, 'input.txt')).toString().split(',').map(Number);
-let prevX, x = Math.min(...input);
-let prevY = input.reduce((acc, n) => acc + ((n === x) ? 0 : -(((n - x) * Math.abs(n + 1 - x)) / (2 * Math.abs(n - x)))) + (((n + 1) === x) ? 0 : -((((n + 1 - x) * Math.abs(n - x)) / (2 * Math.abs(n + 1 - x))))), 0);
-let horPos;
-for (x = Math.min(...input) + 1; x <= Math.max(...input); x++) {
-    let y = input.reduce((acc, n) => acc + ((n === x) ? 0 : -(((n - x) * Math.abs(n + 1 - x)) / (2 * Math.abs(n - x)))) + (((n + 1) === x) ? 0 : -((((n + 1 - x) * Math.abs(n - x)) / (2 * Math.abs(n + 1 - x))))), 0);
-    if (y === 0) {
-        horPos = parseInt(x);
-        break;
-    } else if (Math.sign(y) != Math.sign(prevY)) {
-        horPos = parseInt(Math.abs(y) < Math.abs(prevY) ? x : prevX) - 1;
-        break;
-    } else {
-        prevX = x;
-        prevY = y;
-    }
-}
-console.log(input.reduce((acc, n) => acc + (n === horPos ? 0 : Math.abs(n - horPos) * (Math.abs(n - horPos) + 1) / 2), 0));
+console.log(Math.min(...Array.from(Array(1 + Math.max(...(input = require('fs').readFileSync(require('path').join(__dirname, 'input.txt')).toString().split(',').map(Number))) - Math.min(...input)).keys()).map(n => n + Math.min(...input)).map(pivot => input.reduce((acc, n) => acc + (n === pivot ? 0 : Math.abs(n - pivot) * (Math.abs(n - pivot) + 1) / 2), 0))));
