8966593992927f3ebd718e8d1a9a185dcb8f9350
Raffaele Fioratto <raffaele.fioratto@gmail.com>
2021-12-07 15:25:08 +0100

    day7 - challenge2 faster solution

--- 2021/day7/challenge2.js
+++ 2021/day7/challenge2.js
@@ -1,6 +1,18 @@
 let input = require('fs').readFileSync(require('path').join(__dirname, 'input.txt')).toString().split(',').map(Number);
-fuels = [];
-for (let x = 0; x <= Math.max(...input); x++) {
-    fuels.push(input.map(n => x === n ? 0 : (Array.from(Array(Math.abs(n - x) + 1).keys()).reduce((acc, n) => acc + n))).reduce((acc, n) => acc + n));
+let prevX, x = Math.min(...input);
+let prevY = input.reduce((acc, n) => acc + ((n === x) ? 0 : -(((n - x) * Math.abs(n + 1 - x)) / (2 * Math.abs(n - x)))) + (((n + 1) === x) ? 0 : -((((n + 1 - x) * Math.abs(n - x)) / (2 * Math.abs(n + 1 - x))))), 0);
+let horPos;
+for (x = Math.min(...input) + 1; x <= Math.max(...input); x += 0.1) {
+    let y = input.reduce((acc, n) => acc + ((n === x) ? 0 : -(((n - x) * Math.abs(n + 1 - x)) / (2 * Math.abs(n - x)))) + (((n + 1) === x) ? 0 : -((((n + 1 - x) * Math.abs(n - x)) / (2 * Math.abs(n + 1 - x))))), 0);
+    if (y === 0) {
+        horPos = parseInt(x);
+        break;
+    } else if (Math.sign(y) != Math.sign(prevY)) {
+        horPos = parseInt(Math.abs(y) < Math.abs(prevY) ? x : prevX) - 1;
+        break;
+    } else {
+        prevX = x;
+        prevY = y;
+    }
 }
-console.log(Math.min(...fuels));
+console.log(input.reduce((acc, n) => acc + (n === horPos ? 0 : Math.abs(n - horPos) * (Math.abs(n - horPos) + 1) / 2), 0));
