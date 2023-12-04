c5ac8b5a2312c4de5f0a18feecf8efbbab236f59
Raffaele Fioratto <rfioratto@deloitte.it>
2023-12-04 22:00:24 +0100

    day4/2023 one liner challenge1

--- 2023/day4/challenge1.js
+++ 2023/day4/challenge1.js
@@ -1,11 +1,1 @@
-const input = require('fs').readFileSync(require('path').join(__dirname, 'input.txt')).toString().split('\n');
-
-let p = input.map(g => {
-    const numbers = g.split(':')[1].split('|');
-    const winningNumbers = numbers[0].trim().match(/[0-9]+/gi).map(Number);
-    const scratchedNumbers = numbers[1].trim().match(/[0-9]+/gi).map(Number);
-
-    return scratchedNumbers.reduce((acc, sn) => winningNumbers.includes(sn) ? !acc ? 1 : acc * 2 : acc, 0);
-});
-
-console.log(p.reduce((acc, v) => acc + v));
+console.log(require('fs').readFileSync(require('path').join(__dirname, 'input.txt')).toString().split('\n').map(g => Object.values({ a: g.split(':')[1].split('|')[0].trim().match(/[0-9]+/gi).map(Number), b: g.split(':')[1].split('|')[1].trim().match(/[0-9]+/gi).map(Number)}).reduce((w, s) => s.reduce((acc, sn) => w.includes(sn) ? !acc ? 1 : acc * 2 : acc, 0))).reduce((acc, v) => acc + v));
