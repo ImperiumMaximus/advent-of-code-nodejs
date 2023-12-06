a5b146351f78b733b3cef4e303427a5ed003b229
Raffaele Fioratto <rfioratto@deloitte.it>
2023-12-06 08:47:23 +0100

    day6/2023 solutions

---
+++ 2023/day6/challenge1.js
@@ -0,0 +1,17 @@
+const input = require('fs').readFileSync(require('path').join(__dirname, 'input.txt')).toString().split('\n');
+
+const times = input[0].match(/[0-9]+/gi).map(Number);
+const recordDistances = input[1].match(/[0-9]+/gi).map(Number);
+
+let res = 1;
+
+for (let i = 0; i < times.length; i++) {
+    let nWins = 0;
+    for (let j = 0, k = times[i]; j <= k; j++) {
+        nWins += (j * (k - j) > recordDistances[i]) ? 1 : 0;
+    }
+
+    res *= nWins;
+}
+
+console.log(res);
